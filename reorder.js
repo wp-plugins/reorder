6068d41e56b323a41eb12c015277b5d48177bb3b
benjitastic <benjitastic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-07 02:49:55 +0000

    delete old version
    git-svn-id: https://plugins.svn.wordpress.org/reorder/trunk@260956 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- reorder.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-    $("#order-posts-list").sortable({
-	   update : function () {
-	   	$("#order-loading span").html('<img src="../wp-content/plugins/reorder/loading.gif" />');
-		var order = $("#order-posts-list").sortable("serialize");
-		$("#order-loading span").load("../wp-content/plugins/reorder/process-sortable.php?"+order);
-      }  , placeholder :"highlight" , forcePlaceholderSize: true, opacity : 0.6, axis: 'y', items: "li"
-    });
-});
